65dcf391a8951d1f06c92675654c2433539e43e5
Ameyaa <50868733+ameyaa-media@users.noreply.github.com>
2025-11-17 12:24:04 +0530

    updated

--- src/constants.ts
+++ src/constants.ts
@@ -1,5 +1,4 @@
-export const SITE_TITLE = 'TechPeople Blog';
-export const SITE_DESCRIPTION =
-  'Articles, stories and tutorials from Tech People';
+export const SITE_TITLE = "Ameyaa-Astro-Blog";
+export const SITE_DESCRIPTION = "The Deep Dive Collective: Contextualized";
 export const HOMEPAGE_ARTICLE_LIMIT = 6;
 export const ARTICLES_PER_PAGE = 6;
